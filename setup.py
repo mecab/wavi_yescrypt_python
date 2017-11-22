e53475ad5a98e46dedc84ccf15941a07ed071500
mecab <mecab@misosi.ru>
2017-11-22 22:48:20 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup, Extension
 
-zny_yescrypt_module = Extension('zny_yescrypt',
+ytn_yescrypt_module = Extension('ytn_yescrypt',
                             sources = ['yescrypt.c'],
                             extra_compile_args=['-march=native', '-funroll-loops', '-fomit-frame-pointer'],
                             include_dirs=['.'])
 
-setup (name = 'zny_yescrypt',
+setup (name = 'ytn_yescrypt',
        version = '1.0',
-       description = 'Bindings for yescrypt proof of work used by bitzeny',
-       ext_modules = [zny_yescrypt_module])
+       description = 'Bindings for yescrypt proof of work used by yenten',
+       ext_modules = [ytn_yescrypt_module])
